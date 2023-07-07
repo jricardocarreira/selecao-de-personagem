2eb013ae05b91e8d83d8594039a86699e83cab8f
José Ricardo Carreira <jricardocarreira@gmail.com>
2023-07-07 15:42:53 -0300

    estrutura HTML e estilização CSS criados

---
+++ src/js/animations.js
@@ -0,0 +1,24 @@
+function animateScript() {
+    document.getElementById("god").style.backgroundPosition = 
+    `0px -0px`;
+    }
+
+/* const n = [[1, 2, 3, 4],[5, 6],[7, 8, 9]]
+const k = 2
+
+var printNumeros = []
+
+n.forEach(function(arrays) {
+    arrays.forEach(function(numeros){
+        printNumeros.push(numeros) 
+    })
+})
+
+var tirarRebarba = printNumeros.slice(0,printNumeros.length-k)
+
+console.log(tirarRebarba) */
+
+ 
+
+
+
